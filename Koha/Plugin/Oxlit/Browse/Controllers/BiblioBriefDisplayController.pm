3213895514d7b5de8ec7f8225fd567ab79d09c66
Diarmuid <diarmuid@clickclickclick.co.uk>
2025-09-08 18:46:41 +0100

    Moved utils functions into separate file

--- Koha/Plugin/Oxlit/Browse/Controllers/BiblioBriefDisplayController.pm
+++ Koha/Plugin/Oxlit/Browse/Controllers/BiblioBriefDisplayController.pm
@@ -24,36 +24,9 @@ use C4::Search      qw( new_record_from_zebra );
 use C4::Languages   qw( getlanguage );
 use CGI qw('-no_undef_params' -utf8 );
 
+use Koha::Plugin::Oxlit::Browse::Utils::MARC qw(extractBiblioFields getMARCRecord);
 use Koha::SearchEngine::Search;
 use Koha::SearchEngine::QueryBuilder;
-
-=head3 getMARCRecord
-
-Extracts and decodes a MARC record from search results
-
-=cut
-
-sub getMARCRecord {
-    my ( $marcresults ) = @_;
-
-    # Validate input
-    if ( !defined $marcresults || !@$marcresults ) {
-        warn "No MARC results provided to getMARCRecord";
-        return;
-    }
-
-    my $marcrecord = new_record_from_zebra(
-        'biblioserver',
-        $marcresults->[0]
-    );
-
-    if ( !defined $marcrecord ) {
-        warn "ERROR DECODING RECORD - $@: " . $marcresults->[0];
-        return;
-    }
-
-    return $marcrecord;
-}
 
 =head1 API
 
@@ -126,31 +99,7 @@ sub get {
         );
     }
 
-    my $biblio = {};
-    $biblio->{author} = $record->subfield('100', 'a') || '';
-    $biblio->{author_dates} = $record->subfield('100', 'd') || '';
-    $biblio->{title} = $record->subfield('245', 'a') || '';
-    $biblio->{title_remainder} = $record->subfield('245', 'b') || '';
-    $biblio->{statement_of_responsibility} = $record->subfield('245', 'c') || '';
-    $biblio->{title_proper} = $record->subfield('246', 'a') || '';
-    $biblio->{title_proper_remainder} = $record->subfield('246', 'b') || '';
-    $biblio->{edition_statement} = $record->subfield('250', 'a') || '';
-    $biblio->{publication_location} = $record->subfield('260', 'a') || '';
-    $biblio->{publisher} = $record->subfield('260', 'b') || '';
-    $biblio->{publication_date} = $record->subfield('260', 'c') || '';
-    $biblio->{extent} = $record->subfield('300', 'a') || '';
-    $biblio->{other_physical_details} = $record->subfield('300', 'b') || '';
-    $biblio->{series_title} = $record->subfield('440', 'a') || '';
-    $biblio->{series_volume_number} = $record->subfield('440', 'v') || '';
-    $biblio->{author_as_subject} = $record->subfield('600', 'a') || '';
-    $biblio->{subject} = $record->subfield('650', 'a') || '';
-    $biblio->{subject_general_subdivision} = $record->subfield('650', 'x') || '';
-    $biblio->{added_author} = $record->subfield('700', 'a') || '';
-    $biblio->{added_author_dates} = $record->subfield('700', 'd') || '';
-    $biblio->{host_item_relationship} = $record->subfield('773', 'g') || '';
-    $biblio->{host_item_title} = $record->subfield('773', 't') || '';
-    $biblio->{uri} = $record->subfield('856', 'u') || '';
-    $biblio->{document_type} = $record->subfield('911', 'a') || '';
+    my $biblio = extractBiblioFields($record);
 
     return $c->render(
         status => 200,
